262ecee3f8917e05a234ada68c6e3370745596fd
siddTalekar <sidd.tale@gmail.com>
2017-03-21 18:58:35 +0530

    header footer completed

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -3,11 +3,25 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         TemplateService.title = "Home"; //This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
 
-        $scope.mySlides = [
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+        // $scope.mySlides = [
+        //     'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+        //     'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+        //     'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+        //     'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+        // ];
+        $scope.clothCat = [{
+                img: 'frontend/img/1.jpg',
+                shirtType: 'casual shirt'
+            },
+            {
+                img: 'frontend/img/1.jpg'
+            },
+            {
+                img: 'frontend/img/1.jpg'
+            },
+            {
+                img: 'frontend/img/1.jpg'
+            }
         ];
     })
     .controller('CheckoutCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
