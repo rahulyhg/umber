bdcbd28f52e8badbd19d6b72db078f61db0026ae
sagarssabat <sagarssabat@gmail.com>
2017-03-25 12:50:00 +0530

    checkout with css done

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,7 +1,19 @@
-myApp.controller('headerCtrl', function ($scope, TemplateService) {
+myApp.controller('headerCtrl', function ($scope, TemplateService, $uibModal) {
     $scope.template = TemplateService;
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         $(window).scrollTop(0);
     });
     $.fancybox.close(true);
+
+    $scope.openLoginModal10 = function () {
+        // alert('click');
+        // console.log("clla");
+        $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/login.html',
+            scope: $scope,
+            size: 'lg',
+            // windowClass: 'modal-content-radi0'
+        });
+    };
 });
