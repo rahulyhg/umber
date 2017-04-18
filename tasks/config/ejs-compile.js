b3442d1ee6e6b928ec52f5a18d9ceccafea5099f
Chintan <chintan@wohlig.com>
2017-04-18 18:27:44 +0530

    done

--- tasks/config/ejs-compile.js
+++ tasks/config/ejs-compile.js
@@ -1,10 +1,11 @@
 module.exports = function (grunt) {
-    var jsFiles = require("../../frontend/files.js");
+    var folderName = grunt.option('target');
+    var jsFiles = require("../../" + folderName + "/files.js");
     var env = require("../../config/env/development.js");
     grunt.config.set('ejs', {
-        frontend: {
-            src: 'views/development.ejs',
-            dest: 'frontend/index.html',
+        ui: {
+            src: 'views/' + folderName + '.ejs',
+            dest: folderName + '/index.html',
             options: {
                 _: require("lodash"),
                 jsFiles: jsFiles,
