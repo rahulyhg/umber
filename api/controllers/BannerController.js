70e050a691df9c2d4d9c69084da7968c0b1ab486
harshad-wohlig <harshad.shirsat@wohlig.com>
2017-05-07 16:25:54 +0530

    Banner controller api

--- api/controllers/BannerController.js
+++ api/controllers/BannerController.js
@@ -1,3 +1,15 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+    getBanner: function (req, res) {
+        if (req.body) {
+            Banner.getBanner(req.body, res.callback);
+        } else {
+            res.json({
+                message: {
+                    data: "Inavlid request!"
+                }
+            })
+        }
+    }
+};
 module.exports = _.assign(module.exports, controller);
