155a12b5d436d02ca39badcb6b30d40d3366cc3e
harshad-wohlig <harshad.shirsat@wohlig.com>
2017-06-17 15:46:31 +0530

    Buy the look api added in controller.

--- api/controllers/BuythelookController.js
+++ api/controllers/BuythelookController.js
@@ -2,6 +2,18 @@ module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
     getEnabledLook: function (req, res) {
         Buythelook.getEnabledLook(res.callback);
+    },
+
+    getBuyTheLookDetails: function (req, res) {
+        if (req.body) {
+            Buythelook.getBuyTheLookDetails(req.body, res.callback);
+        } else {
+            res.json({
+                message: {
+                    data: "Invalid request!"
+                }
+            });
+        }
     }
 };
 module.exports = _.assign(module.exports, controller);
