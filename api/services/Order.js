4ef4a70c0d145c6886d6957d1390467fa9d87ac1
harshad-wohlig <harshad.shirsat@wohlig.com>
2017-06-17 18:28:00 +0530

    Create order & update order addres api created.

--- api/services/Order.js
+++ api/services/Order.js
@@ -37,7 +37,12 @@ var schema = new Schema({
             required: true
         },
         quantity: Number,
-        price: Number
+        price: Number,
+        status: {
+            type: String,
+            enum: ['returned', 'cancelled'],
+            default: ''
+        }
     }],
     totalAmount: {
         type: Number,
@@ -45,9 +50,15 @@ var schema = new Schema({
     },
     discountAmount: Number,
     shippingAmount: Number,
+    paymentMethod: {
+        type: String,
+        enum: ['cod', 'cc', 'dc', 'netbank'],
+        default: 'cod'
+    },
     orderStatus: {
         type: String,
-        enum: ['processing', 'shipped', 'delivered', 'returned', 'cancelled']
+        enum: ['processing', 'shipped', 'delivered', 'returned', 'cancelled'],
+        default: 'processing'
     }
 });
 
@@ -60,6 +71,49 @@ schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
 module.exports = mongoose.model('Order', schema);
 
-var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "userId", "userId"));
-var model = {};
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "user", "user"));
+var model = {
+    createOrderFromCart: function (data, callback) {
+        if (_.isEmpty(data.userId)) {
+            callback({
+                message: "noUserFound"
+            }, null);
+        } else {
+            Cart.getCart(data, function (err, cart) {
+                if (!_.isEmpty(cart)) {
+                    var order = {};
+                    order.orderNo = Math.ceil(Math.random() * 10000000000000);
+                    order.totalAmount = 0;
+                    for (var idx = 0; idx < cart.products.length; idx++) {
+                        var product = cart.products[idx];
+                        var orderData = {
+                            product: mongoose.Types.ObjectId(product.product._id),
+                            quantity: product.quantity,
+                            price: product.quantity * product.product.price
+                        };
+                        order.products.push(data);
+                        order.totalAmount += data.price;
+                        order.status = 'processing';
+                        order.shippingAmount = 0;
+                        order.discountAmount = 0;
+                    }
+                    Order.saveData(order, callback);
+                } else {
+                    callback(err, null);
+                }
+            });
+        }
+    },
+
+    updateOrderAddress: function (data, callback) {
+        Order.findOneAndUpdate({
+            _id: data._id
+        }, {
+            billingAddress: data.billingAddress,
+            shippingAddress: data.shippingAddress
+        }, function (err, data) {
+            callback(err, data);
+        });
+    }
+};
 module.exports = _.assign(module.exports, exports, model);
