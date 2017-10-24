a2ade0a37eb8f0cbeb6418af29b1a7110bbe7b63
snehalthakare <snehal.thakare@wohlig.com>
2017-10-24 18:57:11 +0530

    mailer ui

--- frontend/js/controllers/checkoutCtrl.js
+++ frontend/js/controllers/checkoutCtrl.js
@@ -1902,7 +1902,17 @@ myApp.controller('CheckoutCtrl', function ($scope, OrderService, ProductService,
     ];
 
     //Function for next button which displays the detail tab if a user is logged in or clicks on the next button for checkout 
+    var clickCounter = 0;
     $scope.proceedToDetailTab = function () {
-        $scope.view = "detailTab";
+        alert('mmc');
+        $scope.view = 'detailTab';
+        console.log($scope.view);
+        clickCounter++;
+        if (clickCounter == 2) {
+            alert('clicked');
+            $scope.view = ' ';
+            $scope.view = 'detailTab';
+        }
     };
+    console.log('out', $scope.view);
 })
