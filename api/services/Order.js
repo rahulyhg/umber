1e19fc35d7a122aa0ba1b44f200af86accad7441
NileshWangad <nilesh.wangad@wohlig.com>
2017-11-03 21:58:07 +0530

    payment gateway

--- api/services/Order.js
+++ api/services/Order.js
@@ -211,6 +211,16 @@ var model = {
             .on('end', function () {
                 console.log("This is the end...");
                 callback.writeHeader(200, { "Content-Type": "text/html" });
+
+                // var m = crypto.createHash('md5');
+                // m.update(workingKey)
+                // var key = m.digest('buffer');
+                // var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';
+                // var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+                // var decoded = decipher.update(formbody, 'hex', 'utf8');
+                // decoded += decipher.final('utf8');
+                // return decoded;
+                console.log("formbody", formbody);
                 callback.write(formbody);
                 callback.end();
             });
