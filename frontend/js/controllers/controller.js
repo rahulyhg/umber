4e5357278716624c498cd0540dfeae343cf13033
snehalthakare <snehal.thakare@wohlig.com>
2017-11-17 19:11:01 +0530

    innerblog int done

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -523,36 +523,33 @@ myApp
         TemplateService.title = "Blogs"; //This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
         var blogData = {}; // To push $stateParams obj
-        var alsoViewBlogData = [];
-        console.log($stateParams);
+        $scope.sliderBlogsData = []; // To push individual blogs plus removed one 
+        // console.log($stateParams);
         if ($stateParams) {
             blogData.id = $stateParams.id;
+            // console.log(blogData.id);
         }
-        console.log('Blog data', blogData);
+        //  console.log('Blog data', blogData);
         NavigationService.getEnabledInnerBlogs(blogData, function (data) {
             $scope.innarBlogs = data.data.data;
-            // console.log('innerblogs', $scope.innarBlogs);
-        });
-        NavigationService.getEnabledBlogs(function (data) {
-            $scope.otherBlogs = data.data.data;
-            $scope.tempData = $scope.otherBlogs;
-            $scope.tempData = _.remove($scope.otherBlogs, function (n) {
-                return blogData.id != n._id;
+            $scope.sliderBlogsData.push($scope.innarBlogs);
+            //  console.log('innerblogs', $scope.innarBlogs);
+            //This service used for view also blogs section.
+            NavigationService.getEnabledBlogs(function (data) {
+                $scope.otherBlogs = data.data.data; // used to get all array of objects from blogs
+                // In $scope.removedBlog we will get the objects except the reoved one
+                $scope.removedBlog = _.remove($scope.otherBlogs, function (n) {
+                    return blogData.id != n._id;
+                });
+                $scope.sliderBlogsData.push($scope.removedBlog);
+                $scope.sliderBlogsData = _.flattenDeep($scope.sliderBlogsData);
+                //console.log('after removing', $scope.removedBlog);
+                // console.log('sliderBlogsData', $scope.sliderBlogsData);
             });
-            // _.forEach($scope.otherBlogs,  function (temp, key)  {  
-            //     console.log("blogData.id", blogData.id, " $scope.otherBlogs._id", temp._id, 'key', key);
-            //     console.log("$scope.otherBlogs._id", $scope.otherBlogs[key])
-            //     if (blogData.id == temp._id) {
-
-            //         // alsoViewBlogData.push({});
-            //         // $scope.tempData.splice(key);
-            //         _.slice(array, [start = 0], [end = array.length])
-            //         console.log("temp ", temp, 'matched key', key, 'matched', $scope.otherBlogs[key]);
-            //     }
-            // });
-            $scope.otherBlogs = $scope.tempData;
-            console.log('innerblogs', $scope.otherBlogs);
         });
+
+
+
 
     })
     //Example API Controller
