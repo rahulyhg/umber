1c7ae29348ec6a5289ec2444b0b3d7279d60eebd
Bhargavpurohit <bhargav.purohit@wohlig.com>
2017-11-18 17:31:52 +0530

    gateway

--- api/services/Order.js
+++ api/services/Order.js
@@ -268,18 +268,18 @@ var model = {
           "encRequest":encRequest,
           "access_code":accessCode
       }
-      reqdata = JSON.stringify(reqdata);
-      formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
-      var url = {
-        url: 'https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction',
-         body: reqdata
-         };
-         console.log(url)
-      request.post(url, function(error, response, body){
-            console.log(error,body);
-            resp.send(body)
-    });
-   
+    //   reqdata = JSON.stringify(reqdata);
+      formbody = '<form id="nonseamless" method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
+    //   var url = {
+    //     url: 'https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction',
+    //      body: reqdata
+    //      };
+    //      console.log(url)
+    //   request.post(url, function(error, response, body){
+    //         console.log(error,body);
+    //         resp.send(body)
+    // });
+   resp.send(formbody,reqdata);
     },
     createOrderFromCart: function (data, callback) {
         // console.log("In createorderfromcart", data);
