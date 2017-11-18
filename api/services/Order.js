8abaacc6a879e12e14c2d0858ccc3496af9e548a
Bhargavpurohit <bhargav.purohit@wohlig.com>
2017-11-18 15:46:14 +0530

    gateway

--- api/services/Order.js
+++ api/services/Order.js
@@ -268,16 +268,20 @@ var model = {
           "encRequest":encRequest,
           "access_code":accessCode
       }
-      reqdata = new Buffer.from(body.toString());;
-      var url = {
-        url: 'https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction',
-         body: reqdata
-         };
-         console.log(url)
-      request.post(url, function(error, response, body){
-            console.log(error,body);
-            resp.send(body)
-    });
+      reqdata = new Buffer.from(body.toString());
+      formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
+    //   var url = {
+    //     url: 'https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction',
+    //      body: reqdata
+    //      };
+    //      console.log(url)
+    //   request.post(url, function(error, response, body){
+    //         console.log(error,body);
+    //         resp.send(body)
+    // });
+    resp.writeHeader(200, {"Content-Type": "text/html"});
+	resp.write(formbody);
+	resp.end();
     },
     createOrderFromCart: function (data, callback) {
         // console.log("In createorderfromcart", data);
