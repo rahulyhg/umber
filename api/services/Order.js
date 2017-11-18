1027c5d77ee2bae7c1dc45ca21ee77e8e3a371e8
Bhargavpurohit <bhargav.purohit@wohlig.com>
2017-11-18 15:50:51 +0530

    gateway

--- api/services/Order.js
+++ api/services/Order.js
@@ -270,18 +270,16 @@ var model = {
       }
       reqdata = new Buffer.from(body.toString());
       formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
-    //   var url = {
-    //     url: 'https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction',
-    //      body: reqdata
-    //      };
-    //      console.log(url)
-    //   request.post(url, function(error, response, body){
-    //         console.log(error,body);
-    //         resp.send(body)
-    // });
-    resp.writeHeader(200, {"Content-Type": "text/html"});
-	resp.write(formbody);
-	resp.end();
+      var url = {
+        url: 'https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction',
+         body: formbody
+         };
+         console.log(url)
+      request.post(url, function(error, response, body){
+            console.log(error,body);
+            resp.send(body)
+    });
+   
     },
     createOrderFromCart: function (data, callback) {
         // console.log("In createorderfromcart", data);
