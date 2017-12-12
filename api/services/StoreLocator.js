e8584be7c119bc8402946e82c4325d7d783385c6
prajakta <prajakta.kamble@wohlig.com>
2017-12-12 13:21:50 +0530

    Store loctator model in backend with json

---
+++ api/services/StoreLocator.js
@@ -0,0 +1,31 @@
+var schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lat: Number,
+    long: Number,
+    subcity: [{
+        brand: {
+            type: String
+        },
+        name: String,
+        subCity: String,
+        url: String,
+        address: String,
+        contactNo: String,
+        email: {
+            type: String,
+            validate: validators.isEmail()
+        },
+    }]
+});
+
+schema.plugin(deepPopulate, {});
+schema.plugin(uniqueValidator);
+schema.plugin(timestamps);
+module.exports = mongoose.model('StoreLocator', schema);
+
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
+var model = {};
+module.exports = _.assign(module.exports, exports, model);
